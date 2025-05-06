d9dc4de365282c491a5bc84eeb6bbbba546497d5
Nikolay Pianikov <nikolayp@mail.ru>
2025-05-06 20:39:51 +0300

    Update to 2.1.68

--- Composition.cs
+++ Composition.cs
@@ -6,7 +6,7 @@ internal partial class Composition
     // and can be in any part of the compiled code because this is just a hint to set up an object graph.
     private void Setup() => DI.Setup()
         .Bind().As(Singleton).To<ConsoleAdapter>()
-        // Represents a quantum superposition of 2 states: Alive or Dead
+        // Represents quantum superposition of 2 states: Alive or Dead
         .Bind().To((Random random) => (State)random.Next(2))
         .Bind().To<ShroedingersCat>()
         .Bind().To<CardboardBox<TT>>()
